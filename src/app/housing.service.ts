bd8825dca7cce2ef51a7dd107bdd4409d0908c84
Tebogo <tphiri.firi@gmail.com>
2026-03-27 15:29:33 +0200

    Code refactor - made use of the db.json file that has our information. - HTTP used

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -5,17 +5,17 @@ import { HousingLocation } from './housing-location';
   providedIn: 'root'
 })
 export class HousingService {
-  protected housingLocationList: HousingLocation[] = [
-
-  ];
+  url = 'http://localhost:3000/locations'
   constructor() { }
 
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
   }
 
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(location => location.id === id);
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
   }
 
   submitApplication(firstName: string, lastName: string, email: string) {

9d2ec54deb845f8244488d414a6c8d1e88eaf3b2
Tebogo <tphiri.firi@gmail.com>
2026-03-27 14:40:36 +0200

    imported  FormControl, FormGroup, ReactiveFormsModule to make use of forms

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
     <article>
       <img class="listing-photo" [src]="housingLocation?.photo" alt="Exterior of {{housingLocation?.name}}">
@@ -34,6 +35,11 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService: HousingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params["id"]);
